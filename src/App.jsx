3fbade09a54e2b746020fa18de3c7ec625ef0867
kuku <polorbear520@gmail.com>
2025-02-18 13:50:23 +0800

    feat-1: 建立專案環境和架構

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <>
+            <button type="button" className="btn btn-warning">
+                測試
+            </button>
+        </>
+    );
+};
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,65 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath, URL } from 'url';
+
+// https://vite.dev/config/
+export default defineConfig({
+    base: process.env.NODE_ENV === 'production' ? '/PieZza/' : '/',
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: fileURLToPath(new URL('./src', import.meta.url)),
+            },
+            {
+                find: '@assets',
+                replacement: fileURLToPath(
+                    new URL('./src/assets', import.meta.url)
+                ),
+            },
+            {
+                find: '@components',
+                replacement: fileURLToPath(
+                    new URL('./src/components', import.meta.url)
+                ),
+            },
+            {
+                find: '@helper',
+                replacement: fileURLToPath(
+                    new URL('./src/helper', import.meta.url)
+                ),
+            },
+            {
+                find: '@hook',
+                replacement: fileURLToPath(
+                    new URL('./src/hook', import.meta.url)
+                ),
+            },
+            {
+                find: '@layouts',
+                replacement: fileURLToPath(
+                    new URL('./src/layouts', import.meta.url)
+                ),
+            },
+            {
+                find: '@pages',
+                replacement: fileURLToPath(
+                    new URL('./src/pages', import.meta.url)
+                ),
+            },
+            {
+                find: '@routes',
+                replacement: fileURLToPath(
+                    new URL('./src/routes', import.meta.url)
+                ),
+            },
+            {
+                find: '@slices',
+                replacement: fileURLToPath(
+                    new URL('./src/slices', import.meta.url)
+                ),
+            },
+        ],
+    },
+});
